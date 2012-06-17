ffe0ecc248cffe13cd8014c860b160b504dd912b
Daniel Franzen <daniel.b.franzen@gmail.com>
2012-06-17 19:53:42 +0200

    Stepped to 0.1.4. Any scripts and configuration content in dependencies are now included in the final deployable zip. This can be used for e.g. verification scripts. Added verify.sh which will execute all scripts found in scripts/verification.

---
+++ insulae-deployable/src/main/resources/scripts/verify.sh
@@ -0,0 +1,37 @@
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+VERIFICATION_DIR="${SCRIPT_DIR}/verification"
+
+if [ ! -d ${VERIFICATION_DIR} ] ; then
+	echo "Directory ${VERIFICATION_DIR} does not exist."
+	exit 0
+fi
+
+pushd "${SCRIPT_DIR}/.." $@ > /dev/null
+
+executions=0
+failures=0
+for f in `ls -1 ${VERIFICATION_DIR}` ; do
+	echo "Executing $f"
+	echo "----------------"
+	eval ${VERIFICATION_DIR}/$f
+	retcode=$?
+	echo "----------------"
+	
+	if [ $retcode != 0 ]; then
+		let failures=failures+1
+		echo "VERIFICATION SCRIPT FAILED!"
+	fi
+	
+	echo ""
+	
+	let executions=executions+1
+done
+
+
+popd $@ > /dev/null
+
+echo "$executions verification scripts executed."
+if [ $failures != 0 ]; then
+	echo "THERE WERE $failures FAILURES!"
+	exit 1
+fi
