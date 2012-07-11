5dfbbfd61dabd9ecad4057a9227fa2858fa474fb
Daniel Franzen <daniel.b.franzen@gmail.com>
2012-07-11 18:17:06 +0200

    Using snapshot version of common 1.4.0 to get cli input monitoring. Insulae EntryPoint updated to accept instanceName and pass it to the cli.

--- insulae-deployable/src/main/java/com/warspite/insulae/EntryPoint.java
+++ insulae-deployable/src/main/java/com/warspite/insulae/EntryPoint.java
@@ -12,11 +12,14 @@ import com.warspite.insulae.jetty.JettyContainer;
 
 public class EntryPoint {
 	private final static Logger logger = LoggerFactory.getLogger(EntryPoint.class);
-	private final static Cli cli = new Cli("Insulae");
 	private final static SessionKeeper sessionKeeper = new SessionKeeper();
+	private final static String DEFAULT_INSTANCE_NAME = "default";
 
 	public static void main(String[] args) throws Exception
 	{
+		final String instanceName = getInstanceName(args);
+		final Cli cli = new Cli("Insulae", instanceName);
+		
 		try {
 			logger.info("Extracting resources.");
 
@@ -44,5 +47,17 @@ public class EntryPoint {
 		InsulaeDatabase db = creator.create();
 		db.connect();
 		return db;
+	}
+	
+	private static String getInstanceName(String[] args) {
+		if(args.length == 0 || args[0].isEmpty()) {
+			logger.info("Missing instance name (assumed to be first parameter), defaulting to '" + DEFAULT_INSTANCE_NAME + "'.");
+			return DEFAULT_INSTANCE_NAME;
+		}
+		
+		if(args[0].contains(" ") || args[0].contains("?") || args[0].contains("/") || args[0].contains("\\"))
+			throw new IllegalArgumentException("Received instance name '" + args[0] + "', which contains illegal character.");
+		
+		return args[0];
 	}
 }
--- insulae-deployable/src/main/resources/scripts/start.sh
+++ insulae-deployable/src/main/resources/scripts/start.sh
@@ -1,4 +1,4 @@
 SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 pushd "${SCRIPT_DIR}/.."
-java -classpath "./configuration:./libs/*" com.warspite.insulae.EntryPoint
+java -classpath "./configuration:./libs/*" com.warspite.insulae.EntryPoint $1
 popd
