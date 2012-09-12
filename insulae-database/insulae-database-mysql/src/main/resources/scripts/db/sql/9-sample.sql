5b9984806bbfd53897787adc6ef36dea814c9479
Daniel Franzen <daniel.b.franzen@gmail.com>
2012-09-12 20:24:45 +0200

    Added resource sample data. Using insulae-web 0.2.21. This satisfies #52.

---
+++ insulae-database/insulae-database-mysql/src/main/resources/scripts/db/sql/9-sample.sql
@@ -0,0 +1,4 @@
+INSERT INTO Resource (locationId, resourceTypeId) VALUES (12, (SELECT id FROM ResourceType WHERE canonicalName='iron'));
+INSERT INTO Resource (locationId, resourceTypeId) VALUES (14, (SELECT id FROM ResourceType WHERE canonicalName='gold'));
+INSERT INTO Resource (locationId, resourceTypeId) VALUES (14, (SELECT id FROM ResourceType WHERE canonicalName='gems'));
+INSERT INTO Resource (locationId, resourceTypeId) VALUES (15, (SELECT id FROM ResourceType WHERE canonicalName='gems'));
